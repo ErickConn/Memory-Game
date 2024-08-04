1e265db97f610af9fa859923274e9dac7b9029af
Erick Ricardo <erickbbp2005@gmail.com>
2024-08-04 15:09:55 -0300

    Timer, victory, match cards and lost

--- js/game.js
+++ js/game.js
@@ -1,3 +1,7 @@
+let cardsSelected = []
+const audio = new Audio('../images/spike.mp3')
+audio.play()
+
 const verifyPlayerLogin = ()=>{ 
     const player = sessionStorage.getItem('player')
     if(player != null) {
@@ -8,8 +12,9 @@ const verifyPlayerLogin = ()=>{
     
 }
 
-const createCard = (char)=>{
+const createCard = (char, i)=>{
     const card = document.createElement('div')
+    card.setAttribute('id', i)
     card.classList.add('card')
     const front = document.createElement('div')
     front.classList.add('front')
@@ -23,7 +28,11 @@ const createCard = (char)=>{
     const game = document.getElementsByClassName('game')[0]
     game.appendChild(card)
     card.addEventListener('click', ()=>{
-        card.style.transform = 'rotateY(180deg)'
+        if (cardsSelected.length < 2){
+            card.style.transform = 'rotateY(180deg)'
+            cardsSelected.push(i)
+            verifyCardsMatch()
+        }
     })
 }
 
@@ -43,16 +52,46 @@ const chars = [
 ]
 
 const renderCards = ()=>{
+    let i = 0
     const duplicateChars = [...chars, ...chars]
     duplicateChars.sort(() => Math.random() - 0.5)
     duplicateChars.forEach(char => {
-        createCard(char)
+        createCard(char, i)
+        i++
     });
 }
 
-const spikeSound = ()=>{
-    const audio = new Audio('../images/spike.mp3')
-    audio.play()
+const verifyCardsMatch = ()=>{
+    if (cardsSelected.length == 2) {
+        const card1 = document.getElementById(cardsSelected[0])
+        const card2 = document.getElementById(cardsSelected[1])
+        if (card1.firstChild.style.backgroundImage === card2.firstChild.style.backgroundImage && cardsSelected[1] != cardsSelected[0]) {
+            card1.style.opacity = '0.75'
+            card2.style.opacity = '0.75'
+            card1.firstChild.nextSibling.style.backgroundImage = card1.firstChild.style.backgroundImage
+            card2.firstChild.nextSibling.style.backgroundImage = card2.firstChild.style.backgroundImage
+            card1.setAttribute("class", "matched")
+            card2.setAttribute("class", "matched")
+            card1.setAttribute('disabled', 'true')
+            card2.setAttribute('disabled', 'true')
+            cardsSelected = []
+        }else{
+            setTimeout(()=>{
+                card1.style.transform = ''
+                card2.style.transform = ''
+                cardsSelected = []
+            }, 800)
+        }
+    }
+}
+
+const verifyGameState = ()=>{
+    if ([...document.getElementsByClassName('card')].length == 0){
+        audio.pause()
+        clearInterval(interval)
+        alert("Round Victory! Spike Defused")
+        window.location.reload()
+    }
 }
 
 const Home = document.getElementsByClassName('valorant')[0]
@@ -60,6 +99,19 @@ Home.addEventListener('click', ()=>{
     window.location.href = "index.html"
 })
 
+const timeInit = new Date().getTime()
+
+
+const interval = setInterval(()=>{
+    const timeNow = new Date().getTime()
+    const timeLeft = Math.ceil((timeNow - timeInit)/1000)
+    document.getElementsByClassName('timer')[0].innerHTML = `${timeLeft}s`
+    verifyGameState()
+    if (timeLeft == 105){
+        alert("Time's Up! Spike Destroyed")
+        window.location.reload()
+    }
+}, 10)
+
 verifyPlayerLogin()
 renderCards()
-spikeSound()
